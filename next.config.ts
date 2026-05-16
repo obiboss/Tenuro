2f434a6c9fc68a424f175d736f12235c3f9cc61f
obinna akujobi <phoenixz1@yahoo.com>
2026-05-16 08:25:46 +0100

    update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: "/sitemap.xml",
+        has: [{ type: "host", value: "www.boldverseproperty.com" }],
+        destination: "https://boldverseproperty.com/sitemap.xml",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
